39109be4bdda1220773af6355101b42522f3e273
rinka <keuboumarie2007@gmail.com>
2026-04-18 20:12:39 +0100

    Application gestion banque6

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr, Field
+from enum import Enum
 from typing import Optional
 
 # Schémas pour l'Utilisateur
@@ -7,9 +8,14 @@ class UtilisateurUpdate(BaseModel):
     email: Optional[EmailStr] = None
     solde: Optional[float] = Field(None, ge=0)
 
-# Schémas pour le Compte
+# Définition des types autorisés
+class TypeCompte(str, Enum):
+    COURANT = "courant"
+    EPARGNE = "epargne"
+
+# Utilisation dans le schéma de création
 class CompteCreate(BaseModel):
-    type_compte: str = Field(..., example="courant") # courant ou epargne
+    type_compte: TypeCompte = Field(..., description="Le type de compte doit être 'courant' ou 'epargne'")
     user_id: int
 
 # Schémas pour la Transaction
